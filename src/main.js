f7cc62ca678de24e07e01dec1d49e21382da70b5
guilhermearaujoo <ghma.em1@gmail.com>
2022-11-10 01:03:30 -0300

    Add lint and deployed

--- src/main.js
+++ src/main.js
@@ -6,25 +6,25 @@ const myImg = document.querySelector('img');
 const text = document.querySelector('.card-text-p');
 const btnShuffle = document.querySelector('.div-shuffle-button');
 
-btnShuffle.addEventListener('click', () => {
-
-  // random number from 1 to 731 -> characters ids
-  const RANDOM_ID = Math.floor(Math.random() * 900 ) + 1
-  fetch(`https://superheroapi.com/api.php/499483385541533/${RANDOM_ID}`)
-    .then(response => response.json())
-    .then(data => addInformation(data))
-    .catch(error => {
-      Swal.fire({
-        title: "Cannot found this character id",
-        text: "Try again, press the button!",
-        backdrop: "rgba(0, 0, 0, 0.5)",
-      })
-    })
-})
-
 function addInformation({ name, image: { url } }) {
   myImg.src = url;
   myImg.setAttribute('alt', `image of ${name}`);
   myImg.style.visibility = 'visible';
   text.innerText = name;
 }
+const MAX_ID = 735;
+const randonId = () => Math.floor(Math.random() * MAX_ID) + 1;
+
+btnShuffle.addEventListener('click', () => {
+  // random number from 1 to 731 -> characters ids
+  fetch(`https://superheroapi.com/api.php/499483385541533/${randonId()}`)
+    .then((response) => response.json())
+    .then((data) => addInformation(data))
+    .catch((error) => {
+      Swal.fire({
+        title: 'Cannot found this character id',
+        text: `${error.message}`,
+        backdrop: 'rgba(0, 0, 0, 0.5)',
+      });
+    });
+});
